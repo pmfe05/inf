854aacbd90035110c4510db7477fa54ac653e5a5
pmfe05 <lmphu23705@gmail.com>
2024-02-15 20:32:44 +0700

    fix2

--- script.js
+++ script.js
@@ -6,29 +6,3 @@ myLink.addEventListener("click", function() {
     // Hiển thị thông báo
     window.alert("Select below 👇");
 });
-
-// Lấy các phần tử thẻ a
-var facebookLink = document.getElementById("facebookLink");
-var zaloLink = document.getElementById("zaloLink");
-var githubLink = document.getElementById("githubLink");
-var instagramLink = document.getElementById("instagramLink");
-
-// Thêm sự kiện click cho thẻ a
-facebookLink.addEventListener("click", redirectToLink);
-zaloLink.addEventListener("click", redirectToLink);
-githubLink.addEventListener("click", redirectToLink);
-instagramLink.addEventListener("click", redirectToLink);
-
-
-// Hàm xử lý sự kiện click
-function redirectToLink(event) {
-    // Ngăn chặn hành động mặc định của thẻ a (ngăn chặn chuyển hướng)
-    event.preventDefault();
-
-    // Lấy đường link từ thuộc tính href của thẻ a
-    var link = event.currentTarget.getAttribute("href");
-
-    // Chuyển hướng người dùng đến link
-    window.location.href = link;
-}
-
