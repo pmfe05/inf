790a05f121f634446a1f7d420778c11df7394c08
pmfe05 <lmphu23705@gmail.com>
2024-02-15 20:30:59 +0700

    fix

--- script.js
+++ script.js
@@ -19,24 +19,6 @@ zaloLink.addEventListener("click", redirectToLink);
 githubLink.addEventListener("click", redirectToLink);
 instagramLink.addEventListener("click", redirectToLink);
 
-// Lấy phần tử thẻ a
-var zaloLink = document.getElementById("zaloLink");
-
-// Thêm sự kiện click cho thẻ a
-zaloLink.addEventListener("click", function(event) {
-    // Kiểm tra xem người dùng đã click trực tiếp vào icon hay không
-    if (event.target.classList.contains("fa-z")) {
-        // Ngăn chặn hành động mặc định của thẻ a (ngăn chặn chuyển hướng)
-        event.preventDefault();
-
-        // Lấy đường link từ thuộc tính href của thẻ a
-        var link = zaloLink.getAttribute("href");
-
-        // Chuyển hướng người dùng đến link
-        window.location.href = link;
-    }
-});
-
 
 // Hàm xử lý sự kiện click
 function redirectToLink(event) {
